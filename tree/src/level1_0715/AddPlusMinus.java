47d74ddb169a6890b589f5b535a9e0cc724b1ac7
Tree <gmldbs1109@naver.com>
2021-07-14 09:25:34 -0400

    [feat] : 음양 더하기

---
+++ tree/src/level1_0715/AddPlusMinus.java
@@ -0,0 +1,23 @@
+package level1_0715;
+
+public class AddPlusMinus {
+    public int solution(int[] absolutes, boolean[] signs) {
+        int answer = 0;
+        for(int i = 0 ; i < absolutes.length ; i++) {
+            if(signs[i] == true) {
+                answer += absolutes[i];
+            }
+            if(signs[i] == false) {
+                answer -= absolutes[i];
+            }
+        }
+        System.out.println("answer = " + answer);
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        AddPlusMinus a = new AddPlusMinus();
+        a.solution(new int[]{4,7,12}, new boolean[]{true,false,true}); //9
+        a.solution(new int[]{1,2,3}, new boolean[]{false,false,true}); //0
+    }
+}
