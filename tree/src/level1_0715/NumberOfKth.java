510079a2d52b484b74ba8e42efee7ec9257c9e19
Tree <gmldbs1109@naver.com>
2021-07-14 09:12:13 -0400

    [chore] : k번째 수 프린트 내용 수정

--- tree/src/level1_0715/NumberOfKth.java
+++ tree/src/level1_0715/NumberOfKth.java
@@ -1,6 +1,7 @@
 package level1_0715;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class NumberOfKth {
@@ -10,17 +11,14 @@ public class NumberOfKth {
         List<Integer> tempArr = new ArrayList<>();
         for(int i = 0 ; i < commands.length ; i++) {
             for(int j = commands[i][0] - 1 ; j < commands[i][1] ; j++) {
-                System.out.print(array[j] + " ");
                 tempArr.add(array[j]);
             }
-            System.out.println();
             tempArr.sort(null);
-            System.out.println(tempArr.toString());
             answer[i] = tempArr.get(commands[i][2] - 1);
             tempArr.clear();
         }
 
-
+        System.out.println(Arrays.toString(answer));
         return answer;
     }
 
