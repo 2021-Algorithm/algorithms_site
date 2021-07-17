b3309d05e9a03c0ecf171f8cd0a3b052faf7605c
malaheaven <yunhg95@gmail.com>
2021-07-17 15:52:22 +0900

    #9 숫자 문자열과 영단어 python 풀이 solution3 내용 추가

--- robin/python/level1/study2/number_and_string.py
+++ robin/python/level1/study2/number_and_string.py
@@ -1,15 +1,15 @@
-def is_valid_decimal(str):
+def is_valid_decimal(valid_str):
     try:
-        int(str)
+        int(valid_str)
     except ValueError:
         return False
     return True
 
 
 def solution(s):
-    dic = {"zero": 0, "one": 1, "two": 2, "three": 3,
-           "four": 4, "five": 5, "six": 6, "seven": 7,
-           "eight": 8, "nine": 9}
+    alphabet_dic = {"zero": 0, "one": 1, "two": 2, "three": 3,
+                    "four": 4, "five": 5, "six": 6, "seven": 7,
+                    "eight": 8, "nine": 9}
 
     temp = ''
     answer = ''
@@ -21,23 +21,37 @@ def solution(s):
             answer += str(temp)
             temp = ''
 
-        if temp in dic:
-            answer += str(dic.get(temp))
+        if temp in alphabet_dic:
+            answer += str(alphabet_dic.get(temp))
             temp = ''
 
     return int(answer)
 
 
 def solution2(s):
-    dic = {"zero": "0", "one": "1", "two": "2", "three": "3",
-           "four": "4", "five": "5", "six": "6", "seven": "7",
-           "eight": "8", "nine": "9"}
+    alphabet_dic = {"zero": "0", "one": "1", "two": "2", "three": "3",
+                    "four": "4", "five": "5", "six": "6", "seven": "7",
+                    "eight": "8", "nine": "9"}
     answer = s
-    for key, value in dic.items():
+    for key, value in alphabet_dic.items():
         answer = answer.replace(key, value)
+    return int(answer)
+
+
+def solution3(s):
+    alphabet_num_list = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+
+    answer = s
+
+    for idx, val in enumerate(alphabet_num_list):
+        if val in answer:
+            answer = answer.replace(val, str(idx))
+
     return int(answer)
 
 
 if __name__ == '__main__':
     s = "one4seveneight"
     print(solution(s))
+    print(solution2(s))
+    print(solution3(s))
