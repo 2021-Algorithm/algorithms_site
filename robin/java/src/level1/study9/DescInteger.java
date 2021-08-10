6996a15c0de838847ee743be29b0b3ff52708d78
malaheaven <yunhg95@gmail.com>
2021-08-10 23:46:11 +0900

    #65 정수 내림차순으로 배치하기 java 풀이

---
+++ robin/java/src/level1/study9/DescInteger.java
@@ -0,0 +1,20 @@
+package level1.study9;
+
+import java.util.Arrays;
+
+public class DescInteger {
+
+    public static long solution(long n) {
+        char[] chr = String.valueOf(n).toCharArray();
+        Arrays.sort(chr);
+
+        StringBuilder sb = new StringBuilder(String.valueOf(chr));
+
+        return Long.parseLong(sb.reverse().toString());
+    }
+
+    public static void main(String[] args) {
+        int n = 118372;
+        System.out.println(solution(n));
+    }
+}
